84b3d6cd0495534e3e77fd72865ccccda09b890a
Shahaf Frank-Shapir <67577414+Shahaf-F-S@users.noreply.github.com>
2023-06-16 00:58:06 +0300

    Project Update

--- test.py
+++ test.py
@@ -1,7 +1,22 @@
 # test.py
 
+import time
+
+from spinneroo import Spinners, Spinner
+
 def main() -> None:
     """A function to run the main test."""
+
+    spinner = Spinner(
+        title='Program', message='processing',
+        elements=Spinners.line.value['frames'],
+        complete=f"complete", counter=True, clean=True
+    )
+    spinner.spin()
+
+    time.sleep(5)
+
+    spinner.stop()
 # end main
 
 if __name__ == "__main__":
